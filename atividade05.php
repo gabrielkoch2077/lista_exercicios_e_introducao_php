8d9216c88d1dd6b9d386ed0af338b3665218a51d
gabriel henrique koch <gabril_h_koch@estudante.sesisenai.org.br>
2025-07-07 12:47:10 -0300

    Questão 5 - início

---
+++ atividade05.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verificar se dois números formam um número amigo (soma dos divisores de um é igual ao outro)</title>
+</head>
+<body>
+    
+</body>
+</html>
