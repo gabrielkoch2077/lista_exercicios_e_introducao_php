ff9e9e010329802d08b9ed346178a7996bf72808
gabriel henrique koch <gabril_h_koch@estudante.sesisenai.org.br>
2025-07-14 07:44:11 -0300

    Questão 6 - Finalizada

--- atividade06.php
+++ atividade06.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -7,20 +8,25 @@
 </head>
 
 <body>
-
     <form method="POST" action="">
-        <label for="numero_com_divisor">Verifica os divisores de um número inserido:</label>
+        <label for="numero">Exibe todos os divisores de um número:</label>
         <input type="number" id="numero_com_divisor" name="numero_com_divisor" required>
-        <button type="submit" name="verificar_divisores_numero">Verificar</button>
+        <button type="submit" name="mostrar_divisores">Mostrar Divisores</button>
     </form>
 
     <?php
-    
 
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-
-
-    
+        if (isset($_POST['mostrar_divisores'])) {
+            $numero = $_POST['numero_com_divisor'];
+            for($i = 1; $i <= $numero; $i++){
+                if($numero % $i == 0){
+                    echo $i . " ";
+                };
+            };
+            };
+        };
     ?>
 
 </body>
